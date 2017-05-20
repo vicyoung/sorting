9b550fea76a87072828db72b86c099ab364b5660
vicyoung <vicyoung@outlook.com>
2017-05-20 16:26:55 +0900

    Add annotates

--- sorting.cc
+++ sorting.cc
@@ -8,13 +8,14 @@ using namespace std;
 
 // Heapsort
 void heapsort(uint32_t *m_array, const size_t m_size) {
-    /* Implement your code. */
 	uint32_t heap[m_size+1];
-	size_t count = 0;
+	size_t count = 0;		// Count of heap.
 	for(size_t i = 0;i < m_size;i++) {
+		// Make heap, start from index 1.
 		heap[i+1] = *(m_array+i);
 		size_t j = i+1;
 		while(heap[j]<heap[j/2]&&j!=1){
+			// Swapping
 			uint32_t tmp = heap[j];
 			heap[j] = heap[j/2];
 			heap[j/2] = tmp;
@@ -22,15 +23,13 @@ void heapsort(uint32_t *m_array, const size_t m_size) {
 		}
 		count++;
 	}
-/*	for(size_t i = 1;i < m_size+1;i++) {
-		cout<<heap[i]<<' ';}
-	cout<<endl;*/
 
 	for(size_t i = 0;i < m_size;i++) {
+		// Pop heap, assign to m_array.
 		*(m_array+i) = heap[1];
 		size_t j = 1;
 		while(2*j <= count){
-			
+			// Swapping with smaller child.		
 			if(2*j == count || heap[2*j] < heap[2*j+1]){
 				heap[j] = heap[2*j];
 				j = 2*j;			
@@ -41,18 +40,17 @@ void heapsort(uint32_t *m_array, const size_t m_size) {
 			}
 		}
 		if(j != count){
+			// Fill the blank with the last element.
 			heap[j] = heap[count];
 		}
 		while(heap[j]<heap[j/2]&&j!=1){
-              uint32_t tmp = heap[j];
-              heap[j] = heap[j/2];
-              heap[j/2] = tmp;
-              j/=2;
+			// Check the filled element.					
+			uint32_t tmp = heap[j];
+			heap[j] = heap[j/2];
+            heap[j/2] = tmp;
+            j/=2;
         }
 		count--;
-	/*	for(size_t i = 1;i < m_size+1;i++){
-			cout<<heap[i]<<' ';}
-		cout<<endl;*/
 	}
 
 }
