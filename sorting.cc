0358e28656ec9ba02858e88ba1f51bb695a3ec97
vicyoung <vicyoung@outlook.com>
2017-05-20 14:43:18 +0900

    complete heapsort

--- sorting.cc
+++ sorting.cc
@@ -10,6 +10,7 @@ using namespace std;
 void heapsort(uint32_t *m_array, const size_t m_size) {
     /* Implement your code. */
 	uint32_t heap[m_size+1];
+	size_t count = 0;
 	for(size_t i = 0;i < m_size;i++) {
 		heap[i+1] = *(m_array+i);
 		size_t j = i+1;
@@ -19,12 +20,18 @@ void heapsort(uint32_t *m_array, const size_t m_size) {
 			heap[j/2] = tmp;
 			j/=2;
 		}
+		count++;
 	}
+/*	for(size_t i = 1;i < m_size+1;i++) {
+		cout<<heap[i]<<' ';}
+	cout<<endl;*/
+
 	for(size_t i = 0;i < m_size;i++) {
 		*(m_array+i) = heap[1];
 		size_t j = 1;
-		while(heap[2*j]!= 0){
-			if(heap[2*j] < heap[2*j+1]){
+		while(2*j <= count){
+			
+			if(2*j == count || heap[2*j] < heap[2*j+1]){
 				heap[j] = heap[2*j];
 				j = 2*j;			
 			}
@@ -33,13 +40,8 @@ void heapsort(uint32_t *m_array, const size_t m_size) {
 				j = 2*j+1;
 			}
 		}
-		if(heap[j+1]!=0){
-			size_t k = j+1;
-			while(heap[k+1]!=0){
-				k++;
-			}
-			heap[j] = heap[k];
-			heap[k] = 0;
+		if(j != count){
+			heap[j] = heap[count];
 		}
 		while(heap[j]<heap[j/2]&&j!=1){
               uint32_t tmp = heap[j];
@@ -47,6 +49,10 @@ void heapsort(uint32_t *m_array, const size_t m_size) {
               heap[j/2] = tmp;
               j/=2;
         }
+		count--;
+	/*	for(size_t i = 1;i < m_size+1;i++){
+			cout<<heap[i]<<' ';}
+		cout<<endl;*/
 	}
 
 }
